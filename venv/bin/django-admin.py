aa568e647a7006fd62cca39599b1ecf52b7e9405
Alejandro <alexalejandroem@gmail.com>
2019-11-04 21:07:59 +0100

    Initial commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/alejandroem/Documents/Learning/python-vue-crud/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
